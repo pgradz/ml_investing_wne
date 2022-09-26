dfbb423b425749d4d90ea1a9ff7cfb10c5e1f7b6
pgradz <przemyslaw.gradzki@gmail.com>
2022-09-26 15:10:59 +0000

    backtrader added

--- src/ml_investing_wne/config.py
+++ src/ml_investing_wne/config.py
@@ -16,23 +16,26 @@ COST = {
     'USDJPY': 1.5,
     'EURPLN': 30
 }
-pips = COST[currency] # transactional costs
+try:
+    pips = COST[currency] # transactional costs
+except KeyError:
+    pips = 0
 
 userId = 
 password = ""
 
 # for xtb training:
-train_end = datetime.datetime(2021, 9, 30, 0, 0, 0)
-val_end = datetime.datetime(2021, 12, 31, 0, 0, 0)
-test_end = datetime.datetime(2022, 8, 31, 0, 0, 0)
+# train_end = datetime.datetime(2020, 12, 31, 0, 0, 0)
+# val_end = datetime.datetime(2021, 12, 31, 0, 0, 0)
+# test_end = datetime.datetime(2022, 9, 23, 0, 0, 0)
 # train_end = datetime.datetime(2020, 12, 30, 0, 0, 0)
 # val_end = datetime.datetime(2021, 7, 1, 0, 0, 0)
 # test_end = datetime.datetime(2021, 12, 30, 0, 0, 0)
 # for article:
-# train_end = datetime.datetime(2019, 12, 31, 0, 0, 0)
-# val_end = datetime.datetime(2021, 12, 31, 0, 0, 0)
-# # test_end = datetime.datetime(2021, 12, 31, 0, 0, 0)
-# test_end = datetime.datetime(2022, 8, 31, 0, 0, 0)
+train_end = datetime.datetime(2019, 12, 31, 0, 0, 0)
+val_end = datetime.datetime(2021, 12, 31, 0, 0, 0)
+# test_end = datetime.datetime(2021, 12, 31, 0, 0, 0)
+test_end = datetime.datetime(2022, 8, 31, 0, 0, 0)
 
 seq_len = 96
 batch = 128
@@ -44,6 +47,7 @@ steps_ahead = 1
 # configure directories
 package_directory = os.path.dirname(os.path.abspath(__file__))
 raw_data_path = os.path.join(package_directory, 'data', 'raw', 'hist_data')
+raw_data_path_xtb = os.path.join(package_directory, 'data', 'raw', 'xtb')
 raw_data_path_test = os.path.join(package_directory, 'data', 'test')
 processed_data_path = os.path.join(package_directory, 'data', 'processed')
 model_path = os.path.join(package_directory, 'models',
