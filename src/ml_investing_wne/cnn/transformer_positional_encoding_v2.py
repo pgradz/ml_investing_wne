30a582c36249bcdc2d2696f7d6d040d993d7c1b1
Przemyslaw Aleksander Gradzki <przemyslaw.gradzki@sanofi.com>
2022-10-29 19:21:37 +0200

    resolving old local changes with GPC

--- src/ml_investing_wne/cnn/transformer_positional_encoding_v2.py
+++ src/ml_investing_wne/cnn/transformer_positional_encoding_v2.py
@@ -73,8 +73,14 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
 
     # Feed Forward Part
     x = layers.LayerNormalization(epsilon=1e-6)(res)
+<<<<<<< HEAD
     x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1, activation="relu")(x)
     x = layers.Dropout(dropout)(x)
+=======
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+>>>>>>> 56d7f66 (local old changes)
     return x + res
 
 
