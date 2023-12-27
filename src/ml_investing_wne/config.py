4ccdd5d246c228bd8ca2ced34c45675a9600cd96
pgradz <przemyslaw.gradzki@gmail.com>
2023-12-27 20:03:43 +0100

    changes on mac

--- src/ml_investing_wne/config.py
+++ src/ml_investing_wne/config.py
@@ -7,14 +7,14 @@ import datetime
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 run_type = 'crypto' # forex or crypto
-run_subtype = 'cumsum_triple_barrier' #'triple_barrier_time_aggregated','time_aggregated', 'volume_bars', dollar_bars, 'cumsum', cumsum_triple_barrier', 'volume_bars_triple_barrier', dollar_bars_triple_barrier, range_bar, range_bar_triple_barrier
+run_subtype = 'cumsum' #'triple_barrier_time_aggregated','time_aggregated', 'volume_bars', dollar_bars, 'cumsum', cumsum_triple_barrier', 'volume_bars_triple_barrier', dollar_bars_triple_barrier, range_bar, range_bar_triple_barrier
 provider = 'Binance' # hist_data, Bitstamp, Binance
 currency = 'ETHUSDT'
 
 # model parameters
 input_dim = '1d'  # 2d or 1d
 # has to be defined inside tf_models folder
-model = 'keras_tuner_CNN_LSTM' #'keras_tuner_transformer_positional_encoding', resnet_lstm_regularized, transformer_learnable_encoding, lstm, keras_tuner_CNN_LSTM, keras_tuner_LSTM
+model = 'keras_tuner_tsmixer-GAP' #'keras_tuner_transformer_positional_encoding', resnet_lstm_regularized, transformer_learnable_encoding, lstm, keras_tuner_CNN_LSTM, keras_tuner_LSTM
 
 # time bars
 freq = '1min'
@@ -27,7 +27,7 @@ t_final=24
 fixed_barrier=0.06
 
 # cusum andrange bars params
-cumsum_threshold = 0.01
+cumsum_threshold = 0.015
 
 # if we want to skip consequtive sequences, it is configured by seq_stride. If seq_stride = seq_len then there is 0 overlap at expense of many observations dropped
 seq_stride = 1
--- src/ml_investing_wne/cumsum_filter.py
+++ src/ml_investing_wne/cumsum_filter.py
@@ -8,16 +8,12 @@ from ml_investing_wne import config
 from ml_investing_wne.utils import get_logger
 from ml_investing_wne.experiment_factory import create_asset, experiment_factory
 
-random.seed(config.seed)
-np.random.seed(config.seed)
-tf.random.set_seed(config.seed)
-
 logger = get_logger()
 EXCHANGE = 'binance'
-threshold = 0.015
+threshold = 0.01
 output_path='/Users/i0495036/Documents/sandbox/ml_investing_wne/src/ml_investing_wne/data/processed'
 output_path = os.path.join(output_path, f'{EXCHANGE}_{config.currency}')
-range_bar = False
+range_bar = True
 
 
 def cumsum_filter(df,h,range_bar=False):
@@ -35,15 +31,17 @@ def cumsum_filter(df,h,range_bar=False):
     prc_change_index = df.columns.get_loc('prc_change')
     df['group_id'] = 0
     logger.info('Starting cumsum filter')
+    inital_price = df.loc[0, 'close']
     for i in df.index[1:]:
         if range_bar:
-            s_pos,s_neg=s_pos+df.iloc[i, prc_change_index], s_neg+df.iloc[i, prc_change_index]
+            s_pos,s_neg=df.loc[i, 'close']/inital_price -1, df.loc[i, 'close']/inital_price -1
         else:
             s_pos,s_neg=max(0,s_pos+df.iloc[i, prc_change_index]),min(0,s_neg+df.iloc[i, prc_change_index])
         df.loc[i, 'group_id'] = group_id
         if s_neg<-h or s_pos>h:
             group_id += 1
             s_pos,s_neg = 0,0
+            inital_price = df.loc[i, 'close']
         if i in [prc_10, prc25, prc50, prc75]:
             logger.info(f'Progress: {i/df_size}')
 
@@ -64,8 +62,8 @@ def cumsum_filter(df,h,range_bar=False):
 if __name__ == "__main__":
 
     # set the config for time bars and 1 min bars
-    asset = create_asset()
-    experiment = experiment_factory(asset).get_experiment()
+    asset = create_asset(config)
+    experiment = experiment_factory(asset, config).get_experiment()
     df = experiment.df[['open', 'high', 'low', 'close', 'volume']]
     df2, file_name = cumsum_filter(df, threshold, range_bar=range_bar)
     output_path = os.path.join(output_path, file_name)
