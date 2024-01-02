5af1250bc13714e64686f73136de7db9facafdc6
pgradz <przemyslaw.gradzki@gmail.com>
2024-01-02 08:38:05 +0000

    running sensitivity

--- src/ml_investing_wne/run_sensitivity.sh
+++ src/ml_investing_wne/run_sensitivity.sh
@@ -5,17 +5,17 @@ models=("keras_tuner_CNN_LSTM")
 
 data_params=(
 
-  "0.06,0.02,ETHUSDT" \
-  "0.06,0.025,ETHUSDT" \
-  "0.06,0.03,ETHUSDT" \
-  "0.06,0.035,ETHUSDT" \
-  "0.06,0.04,ETHUSDT" \
-  "0.06,0.05,ETHUSDT" \
-  "0.06,0.06,ETHUSDT" \
-  "0.06,0.07,ETHUSDT" \
-  "0.06,0.08,ETHUSDT" \
-  "0.06,0.09,ETHUSDT" \
-  "0.06,0.1,ETHUSDT" \
+  "0.07,0.02,ETHUSDT" \
+  "0.07,0.025,ETHUSDT" \
+  "0.07,0.03,ETHUSDT" \
+  "0.07,0.035,ETHUSDT" \
+  "0.07,0.04,ETHUSDT" \
+  "0.07,0.05,ETHUSDT" \
+  "0.07,0.06,ETHUSDT" \
+  "0.07,0.07,ETHUSDT" \
+  "0.07,0.08,ETHUSDT" \
+  "0.07,0.09,ETHUSDT" \
+  "0.07,0.1,ETHUSDT" \
 )
 
 
